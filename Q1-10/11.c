3b492275621e484f968d729f64e028ccb1dad615
chauhanyash2850-web <chauhanyash2850@gmail.com>
2025-09-18 13:11:05 +0530

    Create 11.c

---
+++ Q1-10/11.c
@@ -0,0 +1,18 @@
+//Write a program to input an integer and check whether it is even or odd using if–else.
+
+#include <stdio.h>
+int main ()
+{
+    int n;
+    printf ("enter a number ");
+    scanf ("%d",&n);
+    if (n%2==0)
+    {
+        printf ("%d is even",n);
+    }
+    else 
+    {
+        printf ("%d is odd",n);
+    }
+    return 0;
+} 
