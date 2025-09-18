87bdab815419550a80b21c8841e729dc363e4069
chauhanyash2850-web <chauhanyash2850@gmail.com>
2025-09-18 13:05:52 +0530

    1.c

--- Q1-10/1.c
+++ Q1-10/1.c
@@ -1,6 +1,20 @@
+//Write a program to input two numbers and display their sum.
+
+
+
+
 #include <stdio.h>
 int main ()
 {
-    printf ("Hello World");
+    float a,b,c;
+    printf("num1=");
+    scanf ("%f",&a);
+    printf("num2=");
+    scanf ("%f",&b);
+    c=a+b;
+    printf ("%f",c);
+
+
     return 0;
+
 }
